e438300167ecb04d6c188f7b7854af89cd243c8a
Маврин <mavrinaroslav037@gmail.com>
2026-02-12 22:13:00 +0300

    Initial commit: add project structure

---
+++ script.js
@@ -0,0 +1,9 @@
+// Основной скрипт проекта
+console.log("Добро пожаловать в мой Git-проект!");
+
+function showMessage(message) {
+  console.log(`Сообщение: ${message}`);
+  alert(message);
+}
+
+export { showMessage };
